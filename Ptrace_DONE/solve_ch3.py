5ecc514465df7aaa70b0e5b1003287dacbdd4e8f
AnduinBrian <36282595+AnduinBrian@users.noreply.github.com>
2018-10-05 17:55:41 +0700

    ok

--- Ptrace_DONE/solve_ch3.py
+++ Ptrace_DONE/solve_ch3.py
@@ -2,20 +2,21 @@ s = list("ksuiealohgy")
 index = 0
 password = ""
 
-# print s
-
 # 1: eax + 4
 index += 4
 password += s[index]
 index = 0
+
 # 2: eax + 5
 index += 5
 password += s[index]
 index = 0
+
 # 3: eax + 1
 index += 1
 password += s[index]
 index = 0
+
 # 4: eax + 0xA
 index += 0xA
 password += s[index]
